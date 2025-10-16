cd4e7611ff13b7b55a07a8dbebc657bda83aa7fa
premjaiswal0117 <premjaiswal0117@gmail.com>
2025-10-16 11:07:16 +0530

    Add initial files

---
+++ Project Base.py
@@ -0,0 +1,103 @@
+class Student:
+
+    def __init__(self,name,marks,roll):
+        self.name=name
+        self.marks=marks
+        self.roll=roll
+    
+    def display(self):
+        return (f"{self.name} | {self.roll} | {self.marks}")
+
+def search_by_roll(roll):
+        for i in student:
+            if i.roll == roll:
+                return i
+        return None
+
+def save_to_file():
+     with open("Student.txt","w") as f:
+            for s in student:
+                f.write(f"{s.name} | {s.roll} | {s.marks}\n")
+student=[]
+
+try:
+    with open("Student.txt", "r") as f:
+        for line in f:
+            name, roll, marks = line.strip().split(" | ")
+            student.append(Student(name, int(marks), int(roll)))
+except FileNotFoundError:
+    pass
+
+while True:
+    menu = int(input("\nWhat do you want to do?\n"
+                         "1. Add student details\n"
+                         "2. Display students\n"
+                         "3. Search name by roll no\n"
+                         "4. Delete student by roll number\n"
+                         "5. Update student marks by roll number\n"
+                         "6. Save and Exit\nChoice: "))
+    
+    if menu == 1:
+        name=input("Enter name of student:")
+        try:
+            roll = int(input("Enter roll of student: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        try:
+            marks = int(input("Enter marks: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        print("\nStudent Added in file")
+        s=Student(name,marks,roll)
+        student.append(s)
+
+    elif menu == 2:
+        if not student:
+            print("No student added yet ")
+        else:
+            for i in student:
+                print(i.display())
+
+    elif menu == 3:
+        try:
+            search=int(input("Enter the roll no of student you want to search:"))
+        except ValueError:
+            print("Invalid input. enter a valid number")
+            continue
+        result=search_by_roll(search)
+        if result:
+            print("Found:",result.name)
+        else:
+            print("Student Not Found")
+
+    elif menu == 4:
+        try:
+            delete=int(input("Enter the roll no of student you want to Delete:"))
+        except ValueError:
+            print("Invalid input. enter a valid number")
+            continue
+        result=search_by_roll(delete)
+        if result:
+            print(f"{result.name} is deleted")
+            student.remove(result)
+        else:
+            print("Student Not Found")
+    elif menu == 5:
+        try:
+            update=int(input("Enter roll no to update:"))
+        except ValueError:
+            print("Invalid input. enter a valid number")
+            continue
+        result=search_by_roll(update)
+        if result:
+            result.marks=int(input("Enter New Marks:"))
+            print("Marks updated for", result.name)
+        else:
+            print("Not found")
+
+    elif menu == 6:
+        save_to_file()
+        print("Records auto-saved. Exiting...")
+        break
